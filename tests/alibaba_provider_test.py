3fc6460dd7c8b8a825bc5b888de07773de2f751c
Rhys <rhys.powell@gmail.com>
2020-10-01 20:37:10 +0000

    wqAdd Alibaba Cloud
    Signed-off-by: Rhys <rhys.powell@gmail.com>

---
+++ tests/alibaba_provider_test.py
@@ -0,0 +1,45 @@
+import pytest   # noqa: F401
+import requests   # noqa: F401
+import responses
+
+from cloud_detect.providers import AlibabaProvider
+
+
+def test_reading_correct_vendor_file():
+    provider = AlibabaProvider()
+    provider.vendor_file = 'tests/provider_files/alibaba'
+    assert provider.check_vendor_file() is True
+
+
+def test_reading_invalid_vendor_file():
+    provider = AlibabaProvider()
+    provider.vendor_file = 'tests/provider_files/gcp'
+    assert provider.check_vendor_file() is False
+    provider.vendor_file = ''
+    assert provider.check_vendor_file() is False
+
+
+@responses.activate
+def test_valid_metadata_server_check():
+    mocking_url = 'http://testing_metadata_url.com'
+    responses.add(
+        responses.GET, 'http://testing_metadata_url.com',
+        content='Alibaba Cloud ECS',
+    )
+
+    provider = AlibabaProvider()
+    provider.metadata_url = mocking_url
+    assert provider.check_metadata_server() is True
+
+
+@responses.activate
+def test_invalid_metadata_server_check():
+    mocking_url = 'http://testing_metadata_url.com'
+    responses.add(
+        responses.GET, 'http://testing_metadata_url.com',
+        content='',
+    )
+
+    provider = AlibabaProvider()
+    provider.metadata_url = mocking_url
+    assert provider.check_metadata_server() is False
