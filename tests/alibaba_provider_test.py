74a7755f3b8cb28eef0188d964aff10bb66f8252
Rhys <rhys.powell@gmail.com>
2020-10-01 21:30:11 +0000

    fix keyword
    Signed-off-by: Rhys <rhys.powell@gmail.com>

--- tests/alibaba_provider_test.py
+++ tests/alibaba_provider_test.py
@@ -24,7 +24,7 @@ def test_valid_metadata_server_check():
     mocking_url = 'http://testing_metadata_url.com'
     responses.add(
         responses.GET, 'http://testing_metadata_url.com',
-        content='Alibaba Cloud ECS',
+        body='Alibaba Cloud ECS',
     )
 
     provider = AlibabaProvider()
@@ -37,7 +37,7 @@ def test_invalid_metadata_server_check():
     mocking_url = 'http://testing_metadata_url.com'
     responses.add(
         responses.GET, 'http://testing_metadata_url.com',
-        content='',
+        body='',
     )
 
     provider = AlibabaProvider()
