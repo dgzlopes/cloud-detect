323fed24bf39f79ee55406eaa42d2252b8684d5d
dennis-pg <dennisgeorge.mec@gmail.com>
2021-06-01 20:16:40 +0530

    Add Oracle cloud provider (#6)
    * Add Oracle cloud provider
    
    Signed-off-by: Dennis P George <dennisgeorge.mec@gmail.com>
    
    * Add Oracle cloud provider
    
    Signed-off-by: Dennis P George <dennisgeorge.mec@gmail.com>
    
    * Add Oracle cloud provider
    
    Signed-off-by: Dennis P George <dennisgeorge.mec@gmail.com>

---
+++ cloud_detect/providers/oci_provider.py
@@ -0,0 +1,37 @@
+import logging
+from pathlib import Path
+
+import requests
+
+from . import AbstractProvider
+
+
+class OCIProvider(AbstractProvider):
+    """
+        Concrete implementation of the Oracle Cloud Infrastructure cloud provider.
+    """
+
+    def __init__(self, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+        self.vendor_file = '/sys/class/dmi/id/chassis_asset_tag'
+
+    def identify(self):
+        """
+            Tries to identify OCI using all the implemented options
+        """
+        self.logger.info('Try to identify OCI')
+        return self.check_vendor_file()
+
+    def check_metadata_server(self):
+        raise NotImplementedError
+
+    def check_vendor_file(self):
+        """
+            Tries to identify OCI provider by reading the file -> /sys/class/dmi/id/chassis_asset_tag
+        """
+        self.logger.debug('Checking OCI vendor file')
+        oci_path = Path(self.vendor_file)
+        if oci_path.is_file():
+            if 'OracleCloud' in open(self.vendor_file).read():
+                return True
+        return False
