55cb441f4c87fa48994d94de58dfaf0c701c500e
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2022-07-11 14:47:05 +0200

    Cut a new release
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ else:
 
 setup(
     name='cloud-detect',
-    version='0.0.8',
+    version='0.0.9',
     description="Module that determines a host's cloud provider",
     long_description=long_description,
     long_description_content_type='text/markdown',
