004f477dd0052c860ad68c3939fa903268ece2ac
Amir Rossert <amir.rossert@safebreach.com>
2022-07-12 08:08:26 +0300

    Fix test Signed-off-by: Amir Rossert <amir.rossert@safebreach.com>

--- tests/oci_provider_test.py
+++ tests/oci_provider_test.py
@@ -3,8 +3,7 @@ from cloud_detect.providers import OCIProvider
 
 def test_reading_correct_vendor_file():
     provider = OCIProvider()
-    provider.vendor_file = 'tests/provider_files/oci'
-    assert provider.check_vendor_file() is True
+    assert provider.check_vendor_file('tests/provider_files/oci') is True
 
 
 def test_reading_invalid_vendor_file():
