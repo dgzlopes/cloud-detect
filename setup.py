95a9ff3a689066f148610386cd661fa7b33ee293
Derek Cordeiro <derekcordeiro@gmail.com>
2023-07-22 08:28:37 +0530

    Updae github actions for py36, add 3.10, 3.11
    Signed-off-by: Derek Cordeiro <derekcordeiro@gmail.com>

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Topic :: Software Development :: Libraries :: Python Modules',
